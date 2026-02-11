df05c278e2190dcdbac04593cb0e1498626bab5a
Adriano Abreu <adrianodeabreu@gmail.com>
2026-02-11 15:02:05 -0300

    feat: Implementa página de detalhes da issue, adiciona a utilidade `getIssue` e vincula os cards da issue às suas respectivas visualizações de detalhes.

--- src/app/(board)/page.tsx
+++ src/app/(board)/page.tsx
@@ -40,7 +40,7 @@ export default async function Board({ searchParams }: BoardProps) {
             </div>
           ) : (
             issues.backlog.map((issue) => (
-              <Card.Root key={issue.id}>
+              <Card.Root href={`/issues/${issue.id}`} key={issue.id}>
                 <Card.Header>
                   <Card.Number>ISS-{issue.issueNumber}</Card.Number>
                   <Card.Title>{issue.title}</Card.Title>
@@ -81,7 +81,7 @@ export default async function Board({ searchParams }: BoardProps) {
             </div>
           ) : (
             issues.todo.map((issue) => (
-              <Card.Root key={issue.id}>
+              <Card.Root href={`/issues/${issue.id}`} key={issue.id}>
                 <Card.Header>
                   <Card.Number>TODO-{issue.issueNumber}</Card.Number>
                   <Card.Title>{issue.title}</Card.Title>
@@ -122,7 +122,7 @@ export default async function Board({ searchParams }: BoardProps) {
             </div>
           ) : (
             issues.in_progress.map((issue) => (
-              <Card.Root key={issue.id}>
+              <Card.Root href={`/issues/${issue.id}`} key={issue.id}>
                 <Card.Header>
                   <Card.Number>INP-{issue.issueNumber}</Card.Number>
                   <Card.Title>{issue.title}</Card.Title>
@@ -156,7 +156,7 @@ export default async function Board({ searchParams }: BoardProps) {
 
         <Section.Content>
           {issues.backlog.map((issue) => (
-            <Card.Root key={issue.id}>
+            <Card.Root href={`/issues/${issue.id}`} key={issue.id}>
               <Card.Header>
                 <Card.Number>DON-{issue.issueNumber}</Card.Number>
                 <Card.Title>{issue.title}</Card.Title>
